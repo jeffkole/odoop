c9b506a979b70ab4c3f5b6c0570fc9f8a4c6f3b8
Jeff Kolesky <jeff@opower.com>
2012-06-25 14:19:14 -0700

    Makes the conf script a bit more generic

--- hbase-filters/conf/hbase-env.sh
+++ hbase-filters/conf/hbase-env.sh
@@ -1,6 +1,9 @@
 # Extra Java CLASSPATH elements.  Optional.
-if [ -d /Users/jeff/Documents/projects/opower/odoop/hbase-filters/target ]; then
-    FILTER_JAR=`ls /Users/jeff/Documents/projects/opower/odoop/hbase-filters/target/deployed-filter-*-bin.jar 2> /dev/null`
+
+# BASH_SOURCE is the path of this file, since it is the source being executed
+TARGET_DIR="$(dirname $(dirname $BASH_SOURCE))/target"
+if [ -d $TARGET_DIR ]; then
+    FILTER_JAR=`ls $TARGET_DIR/deployed-filter-*-bin.jar 2> /dev/null`
     if [ -n "$FILTER_JAR" ]; then
         # Echo to stderr, because the output here comes out when you run "hbase classpath"
         # and this is not usable as a classpath
