adeac2d9569bfe7cd0f8e07ea872d8ba153011a0
Jeff Kolesky <jeff@opower.com>
2012-07-05 15:29:11 -0700

    Better handles columns and constraints

--- hbase-filters/src/main/scala/com/opower/hadoop/hbase/query/QueryParser.scala
+++ hbase-filters/src/main/scala/com/opower/hadoop/hbase/query/QueryParser.scala
@@ -1,22 +1,25 @@
 package com.opower.hadoop.hbase.query
 
+import org.apache.hadoop.hbase.util.Bytes
+
 import scala.util.parsing.combinator.RegexParsers
 
-class QueryParser extends RegexParsers {
-  val queryBuilder : QueryBuilder = new QueryBuilder
+class QueryParser(input : String) extends RegexParsers {
+  val queryBuilder : QueryBuilder = new QueryBuilder(input)
+  parseAll(this.query, input)
 
   def query = selectClause ~ fromClause ~ whereClause.?
 
   def selectClause = scanClause | getClause
   def scanClause = "scan" ~> repsep(columnDefinition, ",") ^^ {
-    case _ => queryBuilder.scan
+    case _ => this.queryBuilder.scan
   }
   def getClause = "get" ~> repsep(columnDefinition, ",") ^^ {
-    case _ => queryBuilder.get
+    case _ => this.queryBuilder.get
   }
 
   def columnDefinition = versionDefinition.? ~ columnFamily ~ ":" ~ columnQualifier ~ timeRange.? ^^ {
-    case v ~ f ~ ":" ~ q ~ t => queryBuilder.addColumnDefinition(v, f, q, t)
+    case v ~ f ~ ":" ~ q ~ t => this.queryBuilder.addColumnDefinition(v, f, q, t)
   }
   def versionDefinition = allVersions | someVersions
   def allVersions = "all" ~ "versions" ~ "of" ^^ { _ => QueryVersions.All }
@@ -29,18 +32,19 @@ class QueryParser extends RegexParsers {
     case _ ~ a ~ _ ~ b => (a, b)
   }
 
-  def fromClause = "from" ~> tableName ^^ { t => queryBuilder.setTableName(t) }
+  def fromClause = "from" ~> tableName ^^ { t => this.queryBuilder.setTableName(t) }
   /**
    * Table names must not start with a '.' or a '-' and may only contain Latin letters or numbers
    * as well as '_', '-', or '.'.
    */
   def tableName = """\w[\w\-.]*""".r
 
-  def whereClause = "where" ~> rowKeyConstraint ^^ { c => queryBuilder.addConstraint(c) }
+  def whereClause = "where" ~> rowKeyConstraint ^^ { c => this.queryBuilder.addConstraint(c) }
   // TODO: Support "rowkey between X and Y" syntax
   def rowKeyConstraint = "rowkey" ~> rowKeyOperator ~ parameter ^^ {
-    case o ~ p => Constraint(o, p)
+    case o ~ p => RowConstraint(o, p)
   }
+  // TODO: how should the individual operators be parsed out?
   def rowKeyOperator = "([<>]=?|=)".r
 
   /** All named parameters are word characters enclosed with { and } */
@@ -52,33 +56,50 @@ class QueryParser extends RegexParsers {
     val Scan, Get = Value
   }
 
-  case class QueryVersions(numVersions : Int) {
-  }
+  case class QueryVersions(numVersions : Int)
 
   object QueryVersions {
     val All = new QueryVersions(Int.MaxValue)
+    val One = new QueryVersions(1)
   }
 
-  case class Constraint(operator : String, parameter : String) {
-  }
+  case class RowConstraint(operator : String, parameter : String)
 
-  class QueryBuilder {
+  case class Column(family : Array[Byte],
+                    qualifier : Array[Byte],
+                    versions : QueryVersions,
+                    timeRange : Option[(String, String)])
+
+  class QueryBuilder(query : String) {
     var tableName : Option[String] = None
     var queryOperation : Option[QueryOperation.Value] = None
+    var columns : List[Column] = Nil
+    var rowConstraints : List[RowConstraint] = Nil
 
     def scan = { this.queryOperation = Some(QueryOperation.Scan) }
+
     def get = { this.queryOperation = Some(QueryOperation.Get) }
+
     def setTableName(tableName : String) = { this.tableName = Some(tableName) }
-    def addColumnDefinition(version : Option[QueryVersions], family : String,
-        qualifier : String, timeRange : Option[(String, String)]) = {
-      printf("version: %s, fam: %s, qual: %s, timerange: %s%n", version, family, qualifier, timeRange)
+
+    def addColumnDefinition(version : Option[QueryVersions],
+                            family : String,
+                            qualifier : String,
+                            timeRange : Option[(String, String)]) = {
+      // TODO: handle hex-encoded bytes in the qualifier
+      this.columns = Column(Bytes.toBytes(family),
+                            Bytes.toBytes(qualifier),
+                            version.getOrElse(QueryVersions.One),
+                            timeRange) :: this.columns
     }
-    def addConstraint(constraint : Constraint) = {
-      printf("constraint: %s%n", constraint)
+
+    def addConstraint(constraint : RowConstraint) = {
+      this.rowConstraints = constraint :: this.rowConstraints
     }
 
     override def toString = {
-      "Operation: %s; Table: %s".format(this.queryOperation, this.tableName)
+      "Operation: %s; Table: %s; Columns: %s; Row constraints: %s".format(
+        this.queryOperation, this.tableName, this.columns, this.rowConstraints)
     }
   }
 }
