2a3d00475a7266f433fb11a2036ccc23e41e83cf
Jeff Kolesky <jeff@opower.com>
2012-07-06 15:29:39 -0700

    Adds negative test of positiveWholeNumber

--- hbase-filters/src/test/scala/com/opower/hadoop/hbase/query/TestQueryParser.scala
+++ hbase-filters/src/test/scala/com/opower/hadoop/hbase/query/TestQueryParser.scala
@@ -7,7 +7,7 @@ import org.junit.Test
 
 class TestQueryParser extends JUnitSuite with ShouldMatchersForJUnit {
   @Test
-  def testPositiveWholeNumber() {
+  def testPositiveWholeNumberMatchesCorrectly() {
     val parser = new QueryParser(null)
     val input = "19"
     val parseResult = parser.parseAll(parser.positiveWholeNumber, input)
@@ -17,6 +17,16 @@ class TestQueryParser extends JUnitSuite with ShouldMatchersForJUnit {
         next.atEnd should be (true)
       }
       case parser.Failure(msg, next) => fail(msg)
+    }
+  }
+
+  @Test
+  def testPositiveWholeNumberDoesNotMatchNegative() {
+    val parser = new QueryParser(null)
+    val input = "-19"
+    val parseResult = parser.parseAll(parser.positiveWholeNumber, input)
+    parseResult match {
+      case parser.Success(result, next) => fail("Should not have found " + result)
     }
   }
 }
