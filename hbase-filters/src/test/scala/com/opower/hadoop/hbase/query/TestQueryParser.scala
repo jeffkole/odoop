0853637e923cd44f8e922eac4b3e1c41add0daef
Jeff Kolesky <jeff@opower.com>
2012-07-06 16:04:30 -0700

    Adds generic methods for parser testing

--- hbase-filters/src/test/scala/com/opower/hadoop/hbase/query/TestQueryParser.scala
+++ hbase-filters/src/test/scala/com/opower/hadoop/hbase/query/TestQueryParser.scala
@@ -3,30 +3,43 @@ package com.opower.hadoop.hbase.query
 import org.scalatest.junit.JUnitSuite
 import org.scalatest.junit.ShouldMatchersForJUnit
 
+import org.junit.Before
 import org.junit.Test
 
 class TestQueryParser extends JUnitSuite with ShouldMatchersForJUnit {
+  var parser : QueryParser = null
+
+  @Before
+  def setUp() {
+    this.parser = new QueryParser(null)
+  }
+
   @Test
   def testPositiveWholeNumberMatchesCorrectly() {
-    val parser = new QueryParser(null)
-    val input = "19"
-    val parseResult = parser.parseAll(parser.positiveWholeNumber, input)
-    parseResult match {
+    runSuccessfulParse[String](parser, parser.positiveWholeNumber, "19", "19")
+  }
+
+  @Test
+  def testPositiveWholeNumberDoesNotMatchNegative() {
+    runFailedParse[String](parser, parser.positiveWholeNumber, "-19")
+  }
+
+  // Cannot have a path-dependent type of `parser.Parser[T]` in the parameter type definition, but we need that
+  // type to match the parameter types of `parser.parseAll`, so the cast is required.
+  private def runSuccessfulParse[T](parser : QueryParser, term : QueryParser#Parser[T], input : String, expected : T) {
+    parser.parseAll(term.asInstanceOf[parser.Parser[T]], input) match {
       case parser.Success(result, next) => {
-        result should equal ("19")
+        result should equal (expected)
         next.atEnd should be (true)
       }
       case parser.Failure(msg, next) => fail(msg)
     }
   }
 
-  @Test
-  def testPositiveWholeNumberDoesNotMatchNegative() {
-    val parser = new QueryParser(null)
-    val input = "-19"
-    val parseResult = parser.parseAll(parser.positiveWholeNumber, input)
-    parseResult match {
-      case parser.Success(result, next) => fail("Should not have found " + result)
+  private def runFailedParse[T](parser : QueryParser, term : QueryParser#Parser[T], input : String) {
+    parser.parseAll(term.asInstanceOf[parser.Parser[T]], input) match {
+      case parser.Success(result, next) => fail("Should not have matched " + result)
+      case parser.Failure(msg, next) => { /* success! */ }
     }
   }
 }
