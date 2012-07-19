51b6ca28fafded83e1024f62d07e2bcbe9d86d06
Jeff Kolesky <jeff@opower.com>
2012-07-19 10:20:07 -0700

    Adds more meat to the IntTestDefaultQueryPlanner test

--- hbase-filters/src/test/java/com/opower/hadoop/hbase/query/IntTestDefaultQueryPlanner.java
+++ hbase-filters/src/test/java/com/opower/hadoop/hbase/query/IntTestDefaultQueryPlanner.java
@@ -3,11 +3,13 @@ package com.opower.hadoop.hbase.query;
 import org.apache.hadoop.conf.Configuration;
 
 import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.HTableInterfaceFactory;
 import org.apache.hadoop.hbase.client.HTablePool;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -17,6 +19,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import com.opower.hadoop.hbase.test.HBaseTestRunner;
 
@@ -34,10 +38,36 @@ public class IntTestDefaultQueryPlanner {
     private static final String FAMILY_A = "familyA";
     private static final String FAMILY_B = "familyB";
     private static final String FAMILY_C = "familyC";
+    private static final byte[][] FAMILIES = new byte[][] {
+        Bytes.toBytes(FAMILY_A),
+        Bytes.toBytes(FAMILY_B),
+        Bytes.toBytes(FAMILY_C),
+    };
+    private static final String[] ROWS = new String[] {
+        "apple",
+        "banana",
+        "orange",
+        "cantaloupe",
+        "strawberry",
+        "nectarine",
+        "apricot",
+        "peach",
+        "watermelon",
+        "cherry",
+    };
+    // qualifier, num versions, timestamp start, timestamp interval
+    private static final Object[][] QUALIFIERS = new Object[][] {
+        { "oneValueA",     1,    100L,    0, },
+        { "oneBalueB",     1,  10000L,    0, },
+        { "fiveValues",    5,    100L,  100, },
+        { "tenValuesA",   10,   1000L,   10, },
+        { "tenValuesB",   10,   1000L, 1000, },
+    };
 
     private static HBaseTestingUtility hbaseTestingUtility;
     private static HTable table;
     private static HTableInterfaceFactory tableFactory;
+    private static int numKeyValues;
 
     private QueryPlanner queryPlanner;
 
@@ -57,49 +87,26 @@ public class IntTestDefaultQueryPlanner {
     @BeforeClass
     public static void setUpClass() throws Exception {
         byte[] tableName = Bytes.toBytes(TABLE_NAME);
-        byte[][] families = new byte[][] {
-            Bytes.toBytes(FAMILY_A),
-            Bytes.toBytes(FAMILY_B),
-            Bytes.toBytes(FAMILY_C),
-        };
-        table = hbaseTestingUtility.createTable(tableName, families, 100);
+        table = hbaseTestingUtility.createTable(tableName, FAMILIES, 100);
         tableFactory = new StaticTableFactory(table);
 
         // Now load a bunch of data into the table
-        String[] rows = new String[] {
-            "apple",
-            "banana",
-            "orange",
-            "cantaloupe",
-            "strawberry",
-            "nectarine",
-            "apricot",
-            "peach",
-            "watermelon",
-            "cherry",
-        };
-        // qualifier, num versions, timestamp start, timestamp interval
-        Object[][] columns = new Object[][] {
-            { "oneValueA",     1,    100L,    0, },
-            { "oneBalueB",     1,  10000L,    0, },
-            { "fiveValues",    5,    100L,  100, },
-            { "tenValuesA",   10,   1000L,   10, },
-            { "tenValuesB",   10,   1000L, 1000, },
-        };
+        numKeyValues = 0;
         table.setAutoFlush(false);
-        for (byte[] family : families) {
-            for (String row : rows) {
+        for (byte[] family : FAMILIES) {
+            for (String row : ROWS) {
                 byte[] rowKey = Bytes.toBytes(row);
                 Put put = new Put(rowKey);
-                for (Object[] column : columns) {
+                for (Object[] qualifiers : QUALIFIERS) {
                     int i = 0;
-                    byte[] qualifier = Bytes.toBytes((String)column[i++]);
-                    int numVersions = (Integer)column[i++];
-                    long start = (Long)column[i++];
-                    int interval = (Integer)column[i++];
+                    byte[] qualifier = Bytes.toBytes((String)qualifiers[i++]);
+                    int numVersions = (Integer)qualifiers[i++];
+                    long start = (Long)qualifiers[i++];
+                    int interval = (Integer)qualifiers[i++];
                     for (int v = 0; v < numVersions; v++) {
                         byte[] value = Bytes.add(rowKey, Bytes.toBytes(Integer.toString(v)));
                         put.add(family, qualifier, start + (v * interval), value);
+                        numKeyValues++;
                     }
                 }
                 table.put(put);
@@ -127,10 +134,27 @@ public class IntTestDefaultQueryPlanner {
     public void testSimple() throws Exception {
         Query query = this.queryPlanner.parse("scan from " + TABLE_NAME);
         assertThat("query is not null", query, is(notNullValue()));
+        int rowCount = 0;
+        int keyValueCount = 0;
         ResultScanner scanner = null;
         try {
             scanner = query.scan();
             assertThat("scanner is not null", scanner, is(notNullValue()));
+            Result result = null;
+            while ((result = scanner.next()) != null) {
+                rowCount++;
+                for (byte[] family : FAMILIES) {
+                    for (Object[] qualifiers : QUALIFIERS) {
+                        byte[] qualifier = Bytes.toBytes((String)qualifiers[0]);
+                        List<KeyValue> keyValues = result.getColumn(family, qualifier);
+                        keyValueCount += keyValues.size();
+                    }
+                }
+            }
+            assertThat("row count is correct", rowCount, equalTo(ROWS.length));
+            // we expect only one version per qualifier
+            assertThat("key value count is correct", keyValueCount,
+                    equalTo(1 * QUALIFIERS.length * ROWS.length * FAMILIES.length));
         }
         finally {
             if (scanner != null) {
