6528bfac9a46a90abf6ed12809dcf3038205d56f
Jeff Kolesky <jeff@opower.com>
2012-07-19 16:04:48 -0700

    Adds tests for timeranges and behavior in a FilterList

--- hbase-filters/src/test/java/com/opower/hadoop/hbase/filter/IntTestColumnVersionTimerangeFilter.java
+++ hbase-filters/src/test/java/com/opower/hadoop/hbase/filter/IntTestColumnVersionTimerangeFilter.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import org.junit.AfterClass;
@@ -96,6 +97,35 @@ public class IntTestColumnVersionTimerangeFilter {
                 expectedResults, 2);
     }
 
+    @Test
+    public void testTimeranges() throws Exception {
+        Object[][] expectedResults = new Object[][] {
+            { "row-A", "d", "qual-B", 4L, "value-B4" },
+            { "row-A", "d", "qual-B", 3L, "value-B3" },
+            { "row-B", "d", "qual-B", 4L, "value-B4" },
+            { "row-B", "d", "qual-B", 3L, "value-B3" },
+        };
+        runFilterAssertions(new ColumnVersionTimerangeFilter(FAMILY, Bytes.toBytes("qual-B"), 100, 3L, 5L),
+                expectedResults, 2);
+    }
+
+    @Test
+    public void testFilterPlaysNicelyInAFilterList() throws Exception {
+        Object[][] expectedResults = new Object[][] {
+            { "row-A", "d", "qual-A", 5L, "value-A5" },
+            { "row-A", "d", "qual-A", 4L, "value-A4" },
+            { "row-A", "d", "qual-B", 3L, "value-B3" },
+            { "row-A", "d", "qual-B", 2L, "value-B2" },
+            { "row-B", "d", "qual-A", 6L, "value-A6" },
+            { "row-B", "d", "qual-A", 5L, "value-A5" },
+            { "row-B", "d", "qual-B", 3L, "value-B3" },
+        };
+        FilterList filterList = new FilterList(FilterList.Operator.MUST_PASS_ONE);
+        filterList.addFilter(new ColumnVersionTimerangeFilter(FAMILY, Bytes.toBytes("qual-A"), 2, 3L, 7L));
+        filterList.addFilter(new ColumnVersionTimerangeFilter(FAMILY, Bytes.toBytes("qual-B"), 3, 1L, 4L));
+        runFilterAssertions(filterList, expectedResults, 2);
+    }
+
     private void runFilterAssertions(Filter filter, Object[][] expectedResults, int expectedNumRows) throws Exception {
         Scan scan = new Scan();
         scan.addFamily(FAMILY);
@@ -124,10 +154,5 @@ public class IntTestColumnVersionTimerangeFilter {
         }
         assertThat("number of rows matches", numRows, is(expectedNumRows));
         assertThat("number of keyvalue matches", numKeyValues, is(expectedResults.length));
-    }
-
-    @Test
-    public void testTimeranges() throws Exception {
-
     }
 }
